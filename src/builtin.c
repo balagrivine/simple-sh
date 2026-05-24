47238575900fd7e9de4f1d4bf7f8986a2ea43792
balagrivine <balagrivine@gmail.com>
2026-05-24 14:00:59 +0300

    implement export builtin command & add ability to echo exported variables

--- src/builtin.c
+++ src/builtin.c
@@ -10,6 +10,10 @@ int handle_builtin(char *command){
 
     if (strncmp(command, "cd ", 3) == 0) {
         return handle_chdir_builtin(command);
+    } else if (strncmp(command, "export ", 3) == 0) {
+        return handle_export_builtin(command);
+    } else if (strncmp(command, "echo ", 3) == 0) {
+        return handle_echo_builtin(command);
     }
 
     return -1;
@@ -31,4 +35,43 @@ int handle_chdir_builtin(const char *command) {
     return chdir(absolute_path);
 
     return -1;
+}
+
+int handle_export_builtin(char *command) {
+    if (!command) {
+        return -1;
+    }
+
+    char *env = command + strlen("export ");
+
+    char *equal = strchr(env, '=');
+    if (!equal) {
+        fprintf(stderr, "export: Invalid syntax\n");
+        return 0;
+    }
+
+    *equal = '\0';
+
+    char *key = env;
+    char *value = equal + 1;
+
+    return setenv(key, value, 1);
+}
+
+int handle_echo_builtin(char *command) {
+    if (!command) {
+        return -1;
+    }
+
+    char *dollar = strchr(command, '$');
+    if (!dollar) {
+        return -1;
+    }
+
+    char *key = dollar + 1;
+
+    char *value =  getenv(key);
+    printf("%s\n", value);
+
+    return 0;
 }
