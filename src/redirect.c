c5fb4b3d369a8425472a9915a090cf7dc2f14a25
balagrivine <balagrivine@gmail.com>
2026-05-26 11:28:26 +0300

    handle case for redirects with >> where output needs to be appended to the redirect file

--- src/redirect.c
+++ src/redirect.c
@@ -12,33 +12,43 @@ setup_redirect(char *command){
         return -1;
     }
 
+    int flags = O_CREAT | O_WRONLY;
+
     char *redirect = strchr(command, '>');
-    if (!redirect){
+    if (!redirect) {
         return 0;
+    }
+
+    // check for another occurence of '>' so that we open the redirect file
+    // in append mode
+    if (*(redirect + 1) == '>') {
+        flags |= O_APPEND;
+    } else {
+        flags |= O_TRUNC;
     }
 
     *redirect = '\0';
 
     redirect++;
-    while (isspace((unsigned char)*redirect)){
+
+    // Skip second > in >>
+    if (*redirect == '>') {
         redirect++;
     }
 
-    if (*redirect == '\0'){
-        fprintf(stderr, "simple-sh: parse error\n");
-        return -1;
+    while (isspace((unsigned char)*redirect)) {
+        redirect++;
     }
 
-    int fd = open(redirect, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (fd == -1){
+    int fd = open(redirect, flags, 0644);
+    if (fd == -1) {
         perror("open");
         return -1;
     }
 
-    int ret = dup2(fd, STDOUT_FILENO);
-    if (ret == -1){
-        close(fd);
+    if (dup2(fd, STDOUT_FILENO) == -1) {
         perror("dup2");
+        close(fd);
         return -1;
     }
 
